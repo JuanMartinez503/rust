edd9369688786b7706ce16321c20f2c6e466a243
Juan Martinez <juanmartinez5678@hotmail.com>
2026-05-02 17:30:49 -0500

    Delete section-1/hello_world directory

--- section-1/hello_world/target/debug/deps/hello_world-bd0b0f0337df5aea.d
+++
@@ -1,5 +0,0 @@
-/Users/juanmartinez/rust/rust/section-1/hello_world/target/debug/deps/hello_world-bd0b0f0337df5aea.d: src/main.rs
-
-/Users/juanmartinez/rust/rust/section-1/hello_world/target/debug/deps/hello_world-bd0b0f0337df5aea: src/main.rs
-
-src/main.rs:
--- section-1/hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/juanmartinez/rust/rust/section-1/hello_world/target/debug/hello_world: /Users/juanmartinez/rust/rust/section-1/hello_world/src/main.rs
